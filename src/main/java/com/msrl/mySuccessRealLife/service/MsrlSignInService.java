3098f43d38bb5ae04e6013af4b1c72d4cb29a8d6
UAVTechnology <uavtechnology24@gmail.com>
2024-02-05 20:29:11 +0530

    05 Feb 2024

--- src/main/java/com/msrl/mySuccessRealLife/service/MsrlSignInService.java
+++ src/main/java/com/msrl/mySuccessRealLife/service/MsrlSignInService.java
@@ -1,26 +1,20 @@
 package com.msrl.mySuccessRealLife.service;
 
 import com.msrl.mySuccessRealLife.constant.UserConstant;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public class MsrlSignInService {
-	
-	UserConstant user = new UserConstant();
-	private String name = null;
-	
-	public String validateSignIn(String msrlId, String password) {
-		if(msrlId.equals(user.msrlId)) {
-			
-			name = user.userName;
-		}
-		
-		if(null != name) {
-			return name;
-		}
-		else 
-		return "Please provide a valid user id";
-		
-	}
-	
-//	public 
-
+    
+    UserConstant user = new UserConstant();
+    String name = null;
+    public String validateSignIn(String msrlId, String password) {
+        if (msrlId.equals(user.msrlId) && password.equals(user.msrlPassword)) {
+            return user.userName;
+        } else {
+            return name;
+        }
+    }
 }
+
