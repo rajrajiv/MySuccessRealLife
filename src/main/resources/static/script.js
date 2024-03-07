9e501eb996ee22bb2d2059fec4f6b0817a255604
rajrajiv <30924772+rajrajiv@users.noreply.github.com>
2024-03-07 17:31:28 +0530

    admin page linked with main

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,3 +2,13 @@ const toggler = document.querySelector(".btn");
 toggler.addEventListener("click",function(){
     document.querySelector("#sidebar").classList.toggle("collapsed");
 });
+
+
+const admin = document.getElementById("admin"); 
+let type = element.getAttribute("type"); 
+ if(type = 'admin'){
+	 admin.style.diplsy="block";
+ }
+ else{
+	 admin.style.display="none";
+ }
